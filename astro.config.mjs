8c0b5ff3062156cded7decec1caf6da5fff71499
Grant Davies <grant.davies@gmail.com>
2026-01-29 10:56:43 -0800

    Custom domain config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://grantdavies.github.io',
+  site: 'https://granttheant.com',
 })
